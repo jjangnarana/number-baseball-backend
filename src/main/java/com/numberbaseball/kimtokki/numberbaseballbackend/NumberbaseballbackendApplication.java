556960c4bf3a15ace23f415eb6f67d981d75a4b5
kimtokki <jjangnarana@gmail.com>
2023-11-13 23:42:44 +0900

    Initial commit

---
+++ src/main/java/com/numberbaseball/kimtokki/numberbaseballbackend/NumberbaseballbackendApplication.java
@@ -0,0 +1,13 @@
+package com.numberbaseball.kimtokki.numberbaseballbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberbaseballbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberbaseballbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numberbaseball/kimtokki/numberbaseballbackend/NumberbaseballbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numberbaseball.kimtokki.numberbaseballbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberbaseballbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
